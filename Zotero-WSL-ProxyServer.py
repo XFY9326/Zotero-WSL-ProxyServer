6d0a30401c1f3bd5f6818da7ebaf4602855c3c3d
XFY9326 <xfy9326@qq.com>
2022-12-13 13:50:40 +0000

    Fix chunked data proxy Enable log by default More pretty output

--- Zotero-WSL-ProxyServer.py
+++ Zotero-WSL-ProxyServer.py
@@ -2,9 +2,9 @@ import os
 import socket
 import sys
 import platform
-import argparse
 from http import HTTPStatus
 from http.client import HTTPConnection
+from typing import Any, Optional
 
 # noinspection PyBroadException
 try:
@@ -14,31 +14,51 @@ except:
 
 from http.server import BaseHTTPRequestHandler
 
-from socketserver import BaseServer
-from typing import Tuple
+__version__ = "0.2"
 
-ENABLE_REQUEST_LOG = False
+ZOTERO_HOST = "127.0.0.1"
+ZOTERO_PORT = 23119
 
 
 def check_system_requirements():
     if platform.system().lower() != "windows":
-        raise RuntimeError("WSL is a function provided by Windows!")
+        sys.stderr.write("WSL is a function provided by Windows!\n")
+        sys.exit(1)
     if os.system("wsl --version >nul 2>nul") != 0:
-        raise RuntimeError("WSL is not enabled or installed in current system!")
+        sys.stderr.write("WSL is not enabled in current system!\n")
+        sys.exit(1)
     if os.system("ipconfig >nul 2>nul") != 0:
-        raise RuntimeError("Can't read network interface info!")
+        sys.stderr.write("Can't read network interface info!\n")
+        sys.exit(1)
+
+
+def get_process_name_by_pid(pid: int) -> Optional[str]:
+    # noinspection PyBroadException
+    try:
+        with os.popen(f"tasklist | findstr \"{pid}\"") as p2:
+            return p2.readline().split()[0].strip()
+    except:
+        return None
 
 
 def check_port_used(host: str):
-    with os.popen(f"netstat -ano | findstr \"{host}:23119\"") as p:
-        if "LISTENING" in p.readline():
-            raise SystemError(f"Port {host}:23119 is already in use!")
+    with os.popen(f"netstat -ano | findstr \"{host}:{ZOTERO_PORT}\"") as p1:
+        line = p1.readline()
+        if "LISTENING" in line:
+            pid = int(line.split()[-1].strip())
+            process_name = get_process_name_by_pid(pid)
+            if process_name is not None:
+                sys.stderr.write(f"{host}:{ZOTERO_PORT} is already in use by '{process_name}'!\n")
+            else:
+                sys.stderr.write(f"{host}:{ZOTERO_PORT} is already in use!\n")
+            sys.exit(1)
 
 
 def get_wsl_host_ip() -> str:
     ip = None
     with os.popen("ipconfig") as p:
         is_right_adapter = False
+        # noinspection PyBroadException
         try:
             for line in p:
                 if "vEthernet (WSL)" in line:
@@ -47,15 +67,17 @@ def get_wsl_host_ip() -> str:
                     ip = line.split(":")[1].strip()
                     break
         except:
-            raise IOError("Can't recognize WSL host IP address format!")
+            sys.stderr.write("Can't recognize WSL host IP address format!\n")
+            sys.exit(1)
     if ip is None:
-        raise SystemError("Can't find WSL host IP address!")
+        sys.stderr.write("Can't find WSL host IP address!\n")
+        sys.exit(1)
     else:
         return ip
 
 
 def check_zotero_connector() -> bool:
-    connection = HTTPConnection(host="127.0.0.1", port=23119)
+    connection = HTTPConnection(host=ZOTERO_HOST, port=ZOTERO_PORT, timeout=2)
     # noinspection PyBroadException
     try:
         connection.request(method="GET", url="/connector/ping")
@@ -68,27 +90,40 @@ def check_zotero_connector() -> bool:
 
 
 class ZoteroProxyHttpHandler(BaseHTTPRequestHandler):
-    def __init__(self, request: bytes, client_address: Tuple[str, int], server: BaseServer):
-        super(ZoteroProxyHttpHandler, self).__init__(request, client_address, server)
+    sys_version = "Python/" + sys.version.split()[0]
+    server_version = "Zotero-WSL-ProxyServer/" + __version__
+
+    # noinspection PyShadowingBuiltins
+    def log_error(self, format: str, *args: Any) -> None:
+        sys.stderr.write("%s - [%s] %s\n" % (self.address_string(), self.log_date_time_string(), format % args))
+
+    # noinspection PyShadowingBuiltins
+    def log_message(self, format: str, *args: Any) -> None:
+        sys.stdout.write("%s - [%s] %s\n" % (self.address_string(), self.log_date_time_string(), format % args))
 
     def _handle_zotero_request(self, method: str, path: str):
         # Solve "Invalid header" problem
         del self.headers["Host"]
-        body = self.rfile.read(int(self.headers.get("Content-Length", 0)))
-        connection = HTTPConnection(host="127.0.0.1", port=23119)
+
+        content_length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile if content_length != 0 else None
+
+        connection = HTTPConnection(host=ZOTERO_HOST, port=ZOTERO_PORT)
         # noinspection PyBroadException
         try:
             connection.request(method=method, url=path, body=body, headers=self.headers)
             with connection.getresponse() as response:
                 # Remove useless headers
-                if ENABLE_REQUEST_LOG:
-                    self.log_request(response.status)
-                self.send_response_only(response.status)
+                self.send_response(response.status)
                 for k, v in response.headers.items():
                     self.send_header(k, v)
                 self.end_headers()
                 self.wfile.write(response.read())
-        except:
+        except BaseException as e:
+            if check_zotero_connector():
+                self.log_error(f"Unknown proxy request error! Msg: {e}")
+            else:
+                self.log_error(f"Zotero is not running! Msg: {e}")
             self.send_error(HTTPStatus.SERVICE_UNAVAILABLE)
         finally:
             connection.close()
@@ -119,47 +154,31 @@ class ZoteroProxyHttpHandler(BaseHTTPRequestHandler):
 
 
 def launch_zotero_proxy_server(host: str):
-    print("\nPreparing server ... ...", end='')
-    try:
-        httpd = HTTPServer(server_address=(host, 23119), RequestHandlerClass=ZoteroProxyHttpHandler)
-    except BaseException as e:
-        print("\nServer start failed!")
-        raise e
+    print("\nLoading server ...", end='')
+    httpd = HTTPServer(server_address=(host, ZOTERO_PORT), RequestHandlerClass=ZoteroProxyHttpHandler)
     # noinspection HttpUrlsUsage
-    host_zotero_url = f"http://{host}:23119"
-    # noinspection HttpUrlsUsage
-    wsl_zotero_url = f"http://{socket.gethostname().lower()}.local:23119"
-    print(f"\rServer launched in WSL: {host_zotero_url}")
-    print(f"WSL url: {wsl_zotero_url}")
-    print(f"WSL ping check: curl -I {wsl_zotero_url}/connector/ping")
+    wsl_zotero_url = f"http://{socket.gethostname().lower()}.local:{ZOTERO_PORT}"
+    serving_socket_info = httpd.socket.getsockname()
+    print(f"\rServing on {serving_socket_info[0]}:{serving_socket_info[1]}")
+    print(f"Zotero WSL url: {wsl_zotero_url}")
+    print(f"Zotero WSL ping check: curl -I {wsl_zotero_url}/connector/ping")
     print("<Press Control-C to exit>")
-    if ENABLE_REQUEST_LOG:
-        print("-------------------- Request Log --------------------")
+    print("-------------------- Request Log --------------------")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt or SystemExit:
-        print("\nExiting ... ...")
+        print("\nKeyboard interrupt received. Exiting ...")
         httpd.server_close()
         sys.exit(0)
 
 
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Zotero WSL proxy server')
-    parser.add_argument('--log', help='Show request log in proxy server', action='store_true')
-    return parser.parse_args()
-
-
 def main():
-    global ENABLE_REQUEST_LOG
-    params = parse_args()
-    ENABLE_REQUEST_LOG = params.log
-
     check_system_requirements()
     wsl_host_ip = get_wsl_host_ip()
-    print("WSL host IP:", wsl_host_ip)
-    check_port_used(wsl_host_ip)
+    print("Windows host IP in WSL:", wsl_host_ip)
     print("Zotero status:", "Running" if check_zotero_connector() else "Not found")
     print("Server type:", HTTPServer.__name__)
+    check_port_used(wsl_host_ip)
     launch_zotero_proxy_server(wsl_host_ip)
 
 
